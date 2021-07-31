d548e545c94044406d05c92004dcdeb529b3f804
조경진 <cho881020@gmail.com>
2021-07-31 14:19:04 +0900

    Glide 라이브러리 설치  - TedPermission 수정 필요

--- app/src/main/java/com/neppplus/library_20210731/MainActivity.kt
+++ app/src/main/java/com/neppplus/library_20210731/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.neppplus.library_20210731
 
+import android.Manifest
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +19,45 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+
+//        callBtn.setOnClickListener {
+//
+////            1. 권한이 있는지 없는지에 따라, 어떻게 행동할지. 방침 저장.
+//            val permissionListener = object : PermissionListener {
+//
+//                override fun onPermissionGranted() {
+//
+////                    권한이 OK 되었을때
+////                    예시 => 실제 전화 연결 진행. Intent(4)
+//
+//                    val myUri = Uri.parse("tel:01011112222")
+//                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+//                    startActivity(myIntent)
+//
+//                }
+//
+//                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//
+////                    권한이 최종 거부되었을때
+////                    예시 => 전화 걸기 실패 안내 토스트.
+//
+//
+////                    this : object 를 가리키게 될 수도 있다. => MainActivity라고 명확히 표시.
+//                    Toast.makeText(this@MainActivity, "권한이 거절되어 통화가 불가능합니다.", Toast.LENGTH_SHORT).show()
+//
+//                }
+//
+//            }
+//
+////            실제 권한 확인 기능 실행.
+//            TedPermission.with(this)
+//                .setPermissionListener( permissionListener )
+//                .setPermissions(Manifest.permission.CALL_PHONE)
+//                .setDeniedMessage("[설정] 에서 전화 권한을 허용해야 연결이 가능합니다.")
+//                .check()
+//
+//        }
+
     }
 }
