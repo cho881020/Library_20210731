f51440060e949c0c273aecef1fcec1116aa760f5
조경진 <cho881020@gmail.com>
2021-07-31 14:35:42 +0900

    Glide 활용 예시

--- app/src/main/java/com/neppplus/library_20210731/MainActivity.kt
+++ app/src/main/java/com/neppplus/library_20210731/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.neppplus.library_20210731
 
-import android.Manifest
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        인터넷에 있는 이미지를 glideTestImg 에 반영
+        Glide.with(this).load("https://dimg.donga.com/wps/NEWS/IMAGE/2019/12/31/99024137.2.jpg").into(glideTestImg)
 
         photoImg.setOnClickListener {
 
